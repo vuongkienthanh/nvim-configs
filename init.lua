93eb34a8cd01fee10e8de18e2388ad80f00b4ebe
vuongkienthanh <thanhstardust@outlook.com>
2022-03-26 17:31:18 +0700

    rm updatetime, add listchars

--- init.lua
+++ init.lua
@@ -11,7 +11,6 @@ set.backup = false
 set.writebackup = false
 set.showmode = false
 set.cmdheight=2
-set.updatetime=300
 set.hidden = true
 set.cursorline = true
 set.shortmess:append('c')
@@ -19,6 +18,9 @@ set.encoding='utf-8'
 set.colorcolumn = "80"
 set.termguicolors = true
 set.list = true
+set.listchars:append("eol:↴")
+set.listchars:append("lead: ")
+set.listchars:append("multispace:…")
 -- tab
 set.expandtab = true
 set.tabstop=2
@@ -32,16 +34,18 @@ require('plugins')
 -- $HOME/.config/nvim/lua/my_remap.lua
 require('my_remap')
 
--- for wsl
-vim.g.clipboard = {
-  name = "win32yank-wsl",
-  copy = {
-    ["+"] = "win32yank.exe -i --crlf",
-    ["*"] = "win32yank.exe -i --crlf"
-  },
-  paste = {
-    ["+"] = "win32yank.exe -o --crlf",
-    ["*"] = "win32yank.exe -o --crlf"
-  },
-  cache_enable = 0,
-}
+-- for wsl clipboard
+if vim.fn.has('wsl') == 1 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf"
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --crlf",
+      ["*"] = "win32yank.exe -o --crlf"
+    },
+    cache_enable = 0,
+  }
+end
