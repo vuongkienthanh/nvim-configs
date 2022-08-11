eb5d85ad3b0ca8e93fd49fdb03ce816aad59da4d
vuongkienthanh <thanhstardust@outlook.com>
2022-08-11 22:11:33 +0700

    fix hostprog

--- init.lua
+++ init.lua
@@ -29,7 +29,7 @@ set.shiftwidth=2
 set.autoindent = true
 set.smartindent = true
 
-vim.g.python3_host_prog = '/usr/bin/python3.8'
+vim.g.python3_host_prog = '/usr/bin/python'
 
 -- ./lua/plugins.lua
 require('plugins')
