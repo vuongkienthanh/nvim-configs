311e2048102cacebed6ef7ec83fdaf618a424ee4
vuongkienthanh <thanhstardust@outlook.com>
2025-03-16 10:51:39 +0700

    update nvim surround

--- plugins/coding.lua
+++ plugins/coding.lua
@@ -3,8 +3,13 @@ local opts = { silent = true }
 return {
     {
         "kylechui/nvim-surround",
+        version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
         event = "VeryLazy",
-        config = true,
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end,
     },
     {
         "windwp/nvim-autopairs",
